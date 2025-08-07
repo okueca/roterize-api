0ee17f821d8f747733a955c40edfcf9c7e824ae7
ariclinis <ariclinis1@gmail.com>
2025-08-07 21:59:37 +0100

    Config MangoDB

--- Gemfile
+++ Gemfile
@@ -11,6 +11,10 @@ gem "puma", ">= 5.0"
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
 
+# For MangoDB
+gem 'mongoid'
+
+
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
 
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,21 @@
 require_relative "boot"
 
-require "rails/all"
+#require "rails/all"
+
+# Add the following instead of rails/all:
+require "rails"
+# Comment out unneeded frameworks
+# require "active_record/railtie" rescue LoadError
+# require "active_storage/engine" rescue LoadError
+require "action_controller/railtie" rescue LoadError
+require "action_view/railtie" rescue LoadError
+require "action_mailer/railtie" rescue LoadError
+require "active_job/railtie" rescue LoadError
+require "action_cable/engine" rescue LoadError
+# require "action_mailbox/engine" rescue LoadError
+# require "action_text/engine" rescue LoadError
+require "rails/test_unit/railtie" rescue LoadError
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,7 +30,8 @@ module RoterizeApi
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
+    
+    config.mongoid.logger = Logger.new(STDERR, :warn)
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
